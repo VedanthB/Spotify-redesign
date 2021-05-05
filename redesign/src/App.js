b9e2d7efdbc8b99717e3985eae702f17633e0836
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-05-05 20:49:51 +0530

    gave the access token to the spotify api (logic)

--- redesign/src/App.js
+++ redesign/src/App.js
@@ -2,7 +2,10 @@ import './App.css';
 import Login from './components/Login';
 import { useEffect, useState } from "react";
 import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from 'spotify-web-api-js'
 
+
+const spotify = new SpotifyWebApi(); // allows us to communicate to the spotify api
 
 function App() {
   const [token, setToken] = useState(null)
@@ -15,6 +18,12 @@ function App() {
 
     if (_token) {
       setToken(_token);
+
+      spotify.setAccessToken(_token)
+
+      spotify.getMe().then(user => {
+        console.log('>>>>', user)
+      })
     }
 
     console.log('i have a token', token)
