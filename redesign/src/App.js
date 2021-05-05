31a8bff1ee8f7890d49aae4d8ad6f82813f932b7
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-05-05 16:57:11 +0530

    logic to check for access token and if true render the home screen

--- redesign/src/App.js
+++ redesign/src/App.js
@@ -1,11 +1,35 @@
 import './App.css';
 import Login from './components/Login';
+import { useEffect, useState } from "react";
+import { getTokenFromUrl } from "./spotify";
+
 
 function App() {
+  const [token, setToken] = useState(null)
+ 
+  useEffect(() => {
+    //to track any change in the access token
+    const hash = getTokenFromUrl();
+
+    const _token = hash.access_token
+
+    if (_token) {
+      setToken(_token);
+    }
+
+    console.log('i have a token', token)
+  }, [])
+
+
   return (
     <div className="App">
- 
-     <Login />
+      {
+        token ? (
+          <h1>I am logged in</h1>
+        ): (
+          <Login />
+        )
+      }
     </div>
   );
 }
