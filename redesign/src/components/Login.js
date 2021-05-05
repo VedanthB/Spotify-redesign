be8350e0edca8897a47d995cbb9473b241d2bb6a
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-05-05 15:50:40 +0530

    ADD: login page

---
+++ redesign/src/components/Login.js
@@ -0,0 +1,15 @@
+import './Login.css'
+
+function Login() {
+    return (
+        <div className="login">
+            <img 
+             src='https://cdn.dribbble.com/users/441326/screenshots/3165191/spotify-gif---oliver-keane.gif'
+             alt='logo'
+            />
+            <a>Login With Spotify</a>
+        </div>
+    )
+}
+
+export default Login
