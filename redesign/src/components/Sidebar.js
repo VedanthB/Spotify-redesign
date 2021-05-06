4a04f367c34e994bd73d4829649f71b4728a5fc5
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-05-06 12:42:51 +0530

    ADD: SidebarOption

--- redesign/src/components/Sidebar.js
+++ redesign/src/components/Sidebar.js
@@ -1,10 +1,19 @@
 import './Sidebar.css'
+import SidebarOption from './SidebarOption'
 
 
 function Sidebar() {
     return (
         <div className='sidebar'>
-            <h1>i am sidebar</h1>
+            <img 
+             className='sidebar__logo'
+             src='https://cdn.dribbble.com/users/441326/screenshots/3165191/spotify-gif---oliver-keane.gif'
+             alt='logo'
+            />
+
+            <SidebarOption title='Home' />
+            <SidebarOption title='Search' />
+            <SidebarOption title='Your Library' />
         </div>
     )
 }
